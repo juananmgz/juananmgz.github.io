f170f12df4c22dd648ee2ab731e47850a646b46b
juananmgz <juananmgz@gmail.com>
2021-11-28 12:27:40 +0100

    Initial commit

---
+++ js/sections/active-section.js
@@ -0,0 +1,61 @@
+var navbar = document.getElementById("navbar");
+
+/* NAVBAR - ON TOP */
+
+var atTop = false;
+var x = "";
+
+document.addEventListener("scroll", e => {
+
+  if (scrollY <= (window.innerHeight * 0.1)) {
+    if (!atTop) {
+        navbar.classList.remove('ftco-navbar-light-scrolled', 'ftco-navbar-light-scrolled-resize', 'bg-none');
+        atTop = true;
+    }
+  } else {
+    navbar.classList.add('ftco-navbar-light-scrolled', 'ftco-navbar-light-scrolled-resize', 'bg-none');
+    atTop = false;
+  }
+});
+
+
+/* SECTIONS SCROLLING LISTENER */
+
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav .container-fluid div ul li');
+
+window.addEventListener("scroll", e => {
+  let current = '';
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if(scrollY >= (sectionTop - sectionHeight / 3)) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLi.forEach(li => {
+    li.classList.remove('active');
+    if(li.id === ("nav-"+current)){
+      li.classList.add('active');
+
+      const pipeLines = document.querySelectorAll('#line-pipe');
+      const contactLines = document.querySelectorAll('#contact-lines-mail-phone');
+
+      if(current === "about") {
+        pipeLines.forEach(pipeLine => {
+          pipeLine.classList.add('line-animate-forward');
+        });
+      }
+      if(current === "contact") {
+        contactLines.forEach(contactLine => {
+          contactLine.classList.add('contact-lines');
+        });
+      }
+    }
+  });
+});
+
+
