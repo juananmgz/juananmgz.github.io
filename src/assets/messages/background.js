8503321ea05849dbb1ae6fc097dd9100d1f9907e
Juanan <juananmgz@gmail.com>
2026-05-10 10:51:59 +0200

    Add logos and images for feedback section

--- src/assets/messages/background.js
+++ src/assets/messages/background.js
@@ -12,8 +12,7 @@ const background = {
             title: "Concentrio AG",
             position: "Front-End Developer",
             duration: "November 2022 – Currently",
-            imgURL:
-              "https://media.licdn.com/dms/image/v2/C4D0BAQFz9GllwPYAfA/company-logo_200_200/company-logo_200_200/0/1630525954491/concentrio_ag_logo?e=1734566400&v=beta&t=zgwmOtasaan6SYhXz6Tj4jLbYpCuO0NAUwaCFKdIHl0",
+            imgURL: "/images/feedback/concentrio-logo.svg",
             desc: `Currently, I'm part of the <span class="highlight marker">Concentrio AG</span> front-end team, where we develop graphical solutions for leading automotive companies. Following an agile workflow, I serve as front-end developer and the UI/UX lead designer.<br/><span class="highlight font-default">My architectural proposals</span> have improved comprehensibility and usability, reducing task completion time and action complexity. One of my proudest achievements is successfully executing a full user experience revamp, resulting in what the client described as a "more usable, fresh, and professional interface"<br/>Since the end of 2024, I've performed as <span class="highlight font-default">Scrum Master</span>, leading the team to achieve a 20% increase in velocity and a 15% improvement in sprint goal completion rate by facilitating Scrum events, removing impediments, and promoting best practices, gaining additional hours per sprint for value-driven development.`,
           },
           {
@@ -21,8 +20,7 @@ const background = {
             title: "Monobo",
             position: "Front-End Developer",
             duration: "April 2022 – September 2022",
-            imgURL:
-              "https://media.licdn.com/dms/image/v2/D560BAQGw2PGWQdA76g/company-logo_200_200/company-logo_200_200/0/1721749750527/monobo_logo?e=1734566400&v=beta&t=NS85D8mLUWmahYaMMMfyeO-sfWWTaviAsScjiCtQw7M",
+            imgURL: "/images/feedback/monobo-logo.png",
             desc: `Previously, I worked at <span class="highlight marker">Monobo</span> as a junior frontend developer. I crafted a boilerplate skeleton project that was used as starting point to create at least three new projects thanks to its attention to detail, setting its reusability as a keystone. <br/>I collaborated with the design team to create responsive and immersive experiences (3D Maps, web animation) that adhere to web standards, accurately replicating prototype-designed interfaces for both websites and email templates with multiplatform support`,
           },
           {
@@ -30,8 +28,7 @@ const background = {
             title: "Esalab",
             position: "Web Apps Developer",
             duration: "February 2021 – July 2021",
-            imgURL:
-              "https://media.licdn.com/dms/image/v2/C4D0BAQGo0oPjNZrymw/company-logo_200_200/company-logo_200_200/0/1630544172694/esalab_expert_systems_and_applications_logo?e=1734566400&v=beta&t=eXt1eLUM6Zszfqqm2cgI5GKEJX_AzhPA8P4DqCtilcM",
+            imgURL: "/images/feedback/esalab-logo.svg",
             desc: `Earlier in my career, I had a researcher experience at <span class="highlight marker">Esalab</span>, where I worked on IoT projects from hardware to software, using all my competencies in engineering and technology. I also gained experience with a range of full-stack development technologies and learned other skills such as electronics and 3D design and printing`,
           },
         ],
@@ -45,8 +42,7 @@ const background = {
             title: "Degrees",
             position: "University education",
             duration: "",
-            imgURL:
-              "https://media.licdn.com/dms/image/v2/C4D0BAQHppUrz76hK-w/company-logo_200_200/company-logo_200_200/0/1630566321936/universidad_de_salamanca_logo?e=1734566400&v=beta&t=Er4Eu1DrcuQ7-0pY44GUoAy_H2-TgD-0iHe5yfanWp0",
+            imgURL: "/images/feedback/usal-logo.png",
             desc: `I hold a <span class="highlight marker">Master's Degree in Agile Web Development</span> from Alcalá de Henares University, with an 8.92 (A-) overall grade, being one of the highest grades of the promotion. <br/>I also hold a <span class="highlight marker">Computer Engineering Degree</span> from Salamanca University, where my final project received a 10.0 Honour Mention (A+), the highest possible recognition. In addition, this project was awarded two financing grants on different governmental academic-related institutions due to its innovative relevance.<br/>I am fluent in <span class="highlight marker">Spanish</span> and <span class="highlight marker">English</span> and have basic knowledge of <span class="highlight marker">German</span>.`,
           },
           {
@@ -54,8 +50,7 @@ const background = {
             title: "Certifications",
             position: "Official courses",
             duration: "",
-            imgURL:
-              "https://media.licdn.com/dms/image/v2/C4D0BAQHppUrz76hK-w/company-logo_200_200/company-logo_200_200/0/1630566321936/universidad_de_salamanca_logo?e=1734566400&v=beta&t=Er4Eu1DrcuQ7-0pY44GUoAy_H2-TgD-0iHe5yfanWp0",
+            imgURL: "/images/feedback/usal-logo.png",
             desc: `Beyond my academic degrees, I have completed official courses to broaden my professional skill set:<ul><li><span class="highlight font-default">Claude Code in Action</span>. <span class="font-italic">Anthropic</span> (2026)</li><li><span class="highlight font-default">Google UX Design — Professional Certificate</span>. <span class="font-italic">Google Career Certificates</span> (2025)</li><li><span class="highlight font-default">Certified ScrumMaster (CSM)</span>. <span class="font-italic">Scrum Alliance</span> (2024)</li><li><span class="highlight font-default">Business &amp; Marketing planning. Industrial &amp; Intellectual properties. Operations and Human Resources strategies</span>. <span class="font-italic">RED TCUE</span> (2021)</li></ul>`,
           },
         ],
@@ -69,8 +64,7 @@ const background = {
             title: "Engineering",
             position: "Recognition in engineering",
             duration: "",
-            imgURL:
-              "https://media.licdn.com/dms/image/v2/C510BAQGr_wjpru9dog/company-logo_200_200/company-logo_200_200/0/1631382475155?e=1734566400&v=beta&t=y410ohwgSnFYCjuZ2-JqhDNivc_Xku9tP6ITLr1uTEU",
+            imgURL: "",
             desc: `During my roadtrip, I’ve been granted with some awards and prices on <span class="highlight marker">engineering</span> tasks. My project E-Gaita was rewarded with maximum degree's grade, but also with two economic and formative grants between 2021 and 2022.<ul><li><span class="highlight font-default">ICE 2021 Business Project (Cultural Entrepreneurship Discipline)</span>. <span class="font-italic">FUES CyL</span> (2022)</li><li><span class="highlight font-default">Honour Mention - Final Degree Project</span>. <span class="font-italic">University of Salamanca</span> (2021)</li><li><span class="highlight font-default">TCUE - Market Oriented Prototypes</span>. <span class="font-italic">Fund. General USAL</span> (2021)</li></ul>`,
           },
           {
@@ -78,8 +72,7 @@ const background = {
             title: "Music",
             position: "Recognition as musician",
             duration: "",
-            imgURL:
-              "https://media.licdn.com/dms/image/v2/C510BAQGr_wjpru9dog/company-logo_200_200/company-logo_200_200/0/1631382475155?e=1734566400&v=beta&t=y410ohwgSnFYCjuZ2-JqhDNivc_Xku9tP6ITLr1uTEU",
+            imgURL: "",
             desc: `Furthermore, it was a pleasure to be recognised as <span class="highlight marker">musician</span> too. In 2019, my region crowned me thanks to my music dedication. Besides, I was on the podium on some national contests.<ul><li><span class="highlight font-default">Traditional Music Award "Young Talent"</span>. <span class="font-italic">Arlafolk</span> (2019)</li><li><span class="highlight font-default">National Traditional Music Contest</span>:<ul><li>2nd ― Casares de Hurdes, Spain (2022)</li><li>1st ― La Huetre, Spain (2016)</li><li>2nd ― La Huetre, Spain (2015)</li><li>1st ― Casares de Hurdes, Spain (2014)</li><li>2nd ― Casares de Hurdes, Spain (2013)</li></ul></li></ul>`,
           },
         ],
